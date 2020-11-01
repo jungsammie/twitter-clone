2c755e5445585ec32e7810ce24034f24aba9604b
jung.sammie <jung.sammie@gmail.com>
2020-11-01 21:27:55 +0900

    2 Implementing Social login, email login, create account and log out feature is done.

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">My Profile</Link>
+        </li>
+      </ul>
+      Hey:)
+    </nav>
+  );
+};
+
+export default Navigation;
