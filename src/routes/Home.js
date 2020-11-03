47fb191cc33294dbe9a9d01edc6a92cf340344cc
jung.sammie <jung.sammie@gmail.com>
2020-11-03 13:27:27 +0900

    4.1 Added attach photo function and preview feature

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,7 @@ import Tweet from "components/Tweet";
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState();
   // const getTweets = async () => {
   //   const dbTweets = await dbService.collection("tweets").get();
   //   dbTweets.forEach(document => {
@@ -41,11 +42,35 @@ const Home = ({ userObj }) => {
     } = event;
     setTweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files }
+    } = event;
+    const attachedImg = files[0];
+    const reader = new FileReader();
+    // when reader finish to load file this event is fired
+    reader.onloadend = (finishedEvent) => {
+      console.log(finishedEvent);
+      const {
+        currentTarget: { result }
+      } = finishedEvent
+      setAttachment(result);
+    };
+    reader.readAsDataURL(attachedImg);
+  }
+const onClearPhoto = (event) => setAttachment(null);
+
   return (
     <div>
       <form>
-        <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
+        <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" onClick={onSubmit} value="Tweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" />
+            <button onClick={onClearPhoto}>Clear</button>
+          </div>)}
       </form>
       <div>
         {tweets.map((tweet) => (
